86e9aa90c466a477a7d50d073a89c46fcb4a91d2
Charles Farris <charlesfarris71@gmail.com>
2026-02-01 09:50:57 -0700

    Scaffold web application

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+app.use("/", serveStatic({ path: "./public/index.html" }));
+
+app.get("/api/ping", (c) => {
+  return c.json({ message: "pong" });
 });
 
 export default {
