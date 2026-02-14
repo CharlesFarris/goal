73304b2f2babb534faafbc5665990ebd6a33a711
Charles Farris <charlesfarris71@gmail.com>
2026-02-14 15:42:52 -0700

    Render home page

--- src/home.tsx
+++ src/home.tsx
@@ -3,5 +3,14 @@ import { Hono } from "hono";
 export const home = new Hono();
 
 home.get("/home", (c) => {
-  return c.json({ message: "home" });
+  return c.html(
+    <html>
+      <head>
+        <title>Goal</title>
+      </head>
+      <body>
+        <h1>Home</h1>
+      </body>
+    </html>,
+  );
 });
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 import { Hono } from "hono";
-import { serveStatic } from "hono/bun";
 import { home } from "./home.tsx";
 import { ping } from "./ping.ts";
 
 export const app = new Hono();
-
-app.use("/", serveStatic({ path: "./public/index.html" }));
 
 app.route("/api", ping);
 app.route("/", home);
