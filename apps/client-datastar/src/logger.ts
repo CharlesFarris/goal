6740a001ece303bb29dceca14cb10425520e69f5
Charles Farris <charlesfarris71@gmail.com>
2026-05-02 17:21:16 -0600

    Add conditional Seq logging

--- apps/client-datastar/src/logger.ts
+++ apps/client-datastar/src/logger.ts
@@ -2,9 +2,32 @@ import { SeqTransport } from "@datalust/winston-seq";
 import winston from "winston";
 
 const seqServerUrlEnv = "SEQ_SERVER_URL";
-const seqServerUrl = Bun.env[seqServerUrlEnv] ?? "";
+const seqServerUrl = Bun.env[seqServerUrlEnv];
 const seqApiKeyEnv = "SEQ_API_KEY";
-const seqApiKey = Bun.env[seqApiKeyEnv] ?? "";
+const seqApiKey = Bun.env[seqApiKeyEnv];
+
+// set default transport
+const transports: winston.transport[] = [
+  new winston.transports.Console({
+    format: winston.format.simple(),
+  }),
+];
+
+// check Seq configuration
+if (seqServerUrl !== undefined && seqApiKey !== undefined) {
+  // add Seq logging
+  transports.push(
+    new SeqTransport({
+      serverUrl: seqServerUrl,
+      apiKey: seqApiKey,
+      onError: (e) => {
+        console.error(e);
+      },
+      handleExceptions: true,
+      handleRejections: true,
+    }),
+  );
+}
 
 export const logger = winston.createLogger({
   level: "info",
@@ -16,18 +39,5 @@ export const logger = winston.createLogger({
   defaultMeta: {
     application: "goal",
   },
-  transports: [
-    new winston.transports.Console({
-      format: winston.format.simple(),
-    }),
-    new SeqTransport({
-      serverUrl: seqServerUrl,
-      apiKey: seqApiKey,
-      onError: (e) => {
-        console.error(e);
-      },
-      handleExceptions: true,
-      handleRejections: true,
-    }),
-  ],
+  transports,
 });
