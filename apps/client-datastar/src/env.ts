1e9ddb49e7966eaa27244a911b14104490fab8b2
Charles Farris <charlesfarris71@gmail.com>
2026-05-02 21:49:01 -0600

    Make env instance readonly

--- apps/client-datastar/src/env.ts
+++ apps/client-datastar/src/env.ts
@@ -1,9 +1,11 @@
 import z from "zod";
 
-const envSchema = z.object({
-  PORT: z.coerce.number().default(3000),
-  SEQ_API_KEY: z.string().optional(),
-  SEQ_SERVER_URL: z.url().optional(),
-});
+const envSchema = z
+  .object({
+    PORT: z.coerce.number().default(3000),
+    SEQ_API_KEY: z.string().optional(),
+    SEQ_SERVER_URL: z.url().optional(),
+  })
+  .readonly();
 
 export const env = envSchema.parse(Bun.env);
